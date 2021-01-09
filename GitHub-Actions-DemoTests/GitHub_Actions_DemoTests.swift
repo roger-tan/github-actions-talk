48b3f472ff1a9ea61ca032c52efc96f5cdf553ec
Roger TAN <roger-tan@users.noreply.github.com>
2021-01-09 15:50:33 +0000

    Initial Commit

---
+++ GitHub-Actions-DemoTests/GitHub_Actions_DemoTests.swift
@@ -0,0 +1,40 @@
+//
+//  GitHub_Actions_DemoTests.swift
+//  GitHub-Actions-DemoTests
+//
+//  Created by Roger Tan on 10/01/2021.
+//
+
+import XCTest
+@testable import GitHub_Actions_Demo
+
+class GitHub_Actions_DemoTests: XCTestCase {
+
+    func testOne() {
+        let subject = "1"
+        let expected = "1"
+        
+        XCTAssertEqual(subject, expected)
+    }
+
+    func testTwo() {
+        let subject = "2"
+        let expected = "2"
+        
+        XCTAssertEqual(subject, expected)
+    }
+
+    func testThree() {
+        let subject = "3"
+        let expected = "3"
+        
+        XCTAssertEqual(subject, expected)
+    }
+    
+    func testFour() {
+        let subject = "4"
+        let expected = "4"
+        
+        XCTAssertEqual(subject, expected)
+    }
+}
---
+++ GitHub-Actions-DemoUITests/GitHub_Actions_DemoUITests.swift
@@ -0,0 +1,34 @@
+//
+//  GitHub_Actions_DemoUITests.swift
+//  GitHub-Actions-DemoUITests
+//
+//  Created by Roger Tan on 10/01/2021.
+//
+
+import XCTest
+
+class GitHub_Actions_DemoUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
