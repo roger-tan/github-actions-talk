9fddf184928c26cc7b68e59390bd5502b954d736
Roger Tan <roger.tan@kinandcarta.com>
2021-05-06 16:57:14 +0100

    Test

--- .github/scripts/environment-demo.sh
+++ .github/scripts/environment-demo.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [[ $1 == "Production" ]];
 then
-    echo "I am running on Production environment"
+    echo "I am running on R E L E A S E branch environment"
 elif [[ $1 == "Staging" ]];
 then
-    echo "I am running on Staging environment"
+    echo "I am running on S T A G I N G branch environment"
 else
     echo "I am running on undefined environment"
 fi
